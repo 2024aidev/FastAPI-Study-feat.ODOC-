e7b6ea34d231656a25c83810a5e2f9bfbd702770
2024aidev <92270317+2024aidev@users.noreply.github.com>
2021-12-01 16:52:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -26,18 +26,18 @@ async def read_item(item_id):
 
 
 @app.get("/add/{var1}/{var2}")
-async def read_item(var1,var2):
+async def add(var1,var2):
     return {'add':int(var1)+int(var2)}
 
 @app.get("/mul/{var1}/{var2}")
-async def read_item(var1,var2):
+async def mul(var1,var2):
     return {'mul':int(var1)*int(var2)}
 
 @app.get("/div/{var1}/{var2}")
-async def read_item(var1,var2):
+async def div(var1,var2):
     return {'div':int(var1)/int(var2)}
 
 @app.get("/sub/{var1}/{var2}")
-async def read_item(var1,var2):
+async def sub(var1,var2):
     return {'sub':abs(int(var1)-int(var2))}
 
